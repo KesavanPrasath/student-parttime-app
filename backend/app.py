c59c714cfe54951681d768c4f142d16a1e7ace0f
KesavanPrasath <k.kesavanprasath@gmail.com>
2026-07-15 17:36:48 +0000

    db_config

---
+++ backend/app.py
@@ -0,0 +1,7 @@
+db_config = {
+    'host': '34.136.166.121',  # Your database's actual public IP address
+    'user': 'root',
+    'password': 'YOUR_ROOT_PASSWORD',  # Replace with the password you created earlier
+    'database': 'shiftsync',
+    'cursorclass': pymysql.cursors.DictCursor
+}
